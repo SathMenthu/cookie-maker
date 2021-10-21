e003a2cb550178f4f69177121b55225877aab452
Mateusz Krasik <4akon4474@gmail.com>
2021-10-21 11:21:58 +0200

    day 2

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const { COOKIE_BASES, COOKIE_ADDONS } = require("../data/cookies-data");
 
 const homeRouter = express.Router();
 
-homeRouter.get("/", (req, res) => res.render("home/index"));
+homeRouter.get("/", (req, res) => {
+  res.render("home/index", {
+    cookie: {
+      base: "dark",
+      addons: ["coconut", "honey"],
+    },
+    bases: Object.entries(COOKIE_BASES),
+    addons: Object.entries(COOKIE_ADDONS),
+    // @TODO: We need something to know what are the prices of selected base and addons..
+  });
+});
 
 module.exports = {
   homeRouter,
